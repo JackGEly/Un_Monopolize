e28cd3245b1e88aeaabe9aad8e5b203fac746ce6
Jenneleh Robinson <jennelehr@gmail.com>
2025-11-08 21:48:02 -0500

    initial commit

---
+++ menu/menu.js
@@ -0,0 +1,8 @@
+document.getElementById("startbutton").addEventListener("click", () => {
+  alert("Timer starts");
+});
+
+document.getElementById("stopbutton").addEventListener("click", () => {
+  alert("Timer stops");
+});
+
